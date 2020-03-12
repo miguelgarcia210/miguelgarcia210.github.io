1a5d3639663fd7fefcc581b01b0bee14c45235b0
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2020-03-11 21:30:01 -0500

    read-more-btn updated and testing safe area

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,8 +8,11 @@ $('.read-more-btn').click(function (e) {
 
     if (extDesc.is(':visible')) {
         $(this).text("Show Less");
+        $(this).css({'border-color':'red', 'background-color':'rgba(58, 62, 72, .7)'});
+
     } else {
         $(this).text("Read More");
+        $(this).css({'border-color':'#04C2C9', 'background-color':'rgba(58, 62, 72, 1)'});
     }
 
 });
