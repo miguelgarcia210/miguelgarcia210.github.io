9c6555fee2d3f0b77443974837845ca42575ddcb
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2020-03-29 01:24:41 -0500

    removed commented code & unused 'complete' function from slideToggle

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,32 +6,6 @@ $('.read-more-btn').click(function (e) {
     let extDesc = $(this).parent().children(".p-desc-cont").children().children("span.extended-description");
     let displayState = extDesc.css('display');
     let thisBtn = $(this);
-    // extDesc.toggle(); // toggle show/hide methods
-    // extDesc.slideToggle('2000', "swing", function () {
-    //     console.log("hello");
-    // });
-
-    // if (extDesc.is(':visible')) { // if toggled to show -> then execute
-    //     $(this).text("Show Less");
-    //     $(this).css({'border-color': 'red', 'background-color': 'rgba(58, 62, 72, .7)'});
-    //
-    // } else { // if toggled to hide -> then execute
-    //     $(this).text("Read More");
-    //     $(this).css({'border-color': '#04C2C9', 'background-color': 'rgba(58, 62, 72, 1)'});
-    //     // Scrolling destinations
-    //     let pImage = $(this).parent().children(".p-image");
-    //     let imagePosition = pImage.offset().top + (-51 - 16); // (-51 accounts for 48px nav height + 3px bottom border) (-16 accounts for padding above)
-    //
-    //     let pContainer = $(this).parent().parent();
-    //     let containerPosition = pContainer.offset().top + (-51);
-    //
-    //     let pContent = $(this).parent();
-    //     let contentPosition = pContent.offset().top + (-51);
-    //
-    //     $('html, body').animate({
-    //         scrollTop: containerPosition
-    //     }, 550);
-    // }
 
     extDesc.slideToggle({ //jQuery slideToggle options
         duration: 'slow',
@@ -39,9 +13,7 @@ $('.read-more-btn').click(function (e) {
             dynamicReadMoreBtn();
             scrollToProjectTopPosition(extDescState());
         },
-        easing: 'swing',
-        complete: function () { // do something once animation is complete
-        }
+        easing: 'swing'
     });
 
     function extDescState() {
