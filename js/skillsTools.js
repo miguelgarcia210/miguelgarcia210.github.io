cdad48d782ec1d2f1abd696534fd61fae1fdeed0
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2020-05-04 21:34:05 -0500

    re-organized functions according to specific sections. updated document.ready functions. created animateTools() which adds animation class to all tool-items then removes class consecutively for future re-occuring animations. created animationToolFunc() which runs animateTools() initially then sets inner-scope interval settings for future modification ease

--- js/skillsTools.js
+++ js/skillsTools.js
@@ -2,31 +2,12 @@
 
 $(function () {
     progress();
+    // animateTools();
+    // animateEveryDuration(10000, animateTools);
+    animationToolFunc();
+
 });
-
-$("#v-lang-header").click(function () {
-    let table =  $("#v-lang-table-container");
-    collapseExpand(table);
-});
-
-$("#equip-header").click(function () {
-    let table = $("#equip-table");
-    collapseExpand(table);
-})
-
-$("#skills-header").click(function () {
-    let skillsBars = $("#skills-bar-container");
-    if (skillsBars.css("display") === "none") {
-        progress();
-    }
-    collapseExpand(skillsBars);
-})
-
-$("#tool-header").click(function () {
-    let toolCatalog = $("#tool-catalog");
-    collapseExpand(toolCatalog);
-})
-
+// GLOBAL FUNCTIONS
 function collapseExpand(elem) {
     // elem
     //     .stop(true, true)
@@ -39,6 +20,69 @@ function collapseExpand(elem) {
         easing: "swing"
     })
 }
+
+// LANGUAGE SECTION
+$("#v-lang-header").click(function () {
+    let table =  $("#v-lang-table-container");
+    collapseExpand(table);
+});
+
+// EQUIPMENT SECTION
+$("#equip-header").click(function () {
+    let table = $("#equip-table");
+    collapseExpand(table);
+})
+
+// SKILLS SECTION
+$("#skills-header").click(function () {
+    let skillsBars = $("#skills-bar-container");
+    if (skillsBars.css("display") === "none") {
+        progress();
+    }
+    collapseExpand(skillsBars);
+})
+
+// TOOLS SECTION
+$("#tool-header").click(function () {
+    let toolCatalog = $("#tool-catalog");
+    if (toolCatalog.css("display") === "none") {
+        animateTools();
+    }
+    collapseExpand(toolCatalog);
+})
+// --- animations ---
+function animationToolFunc() { // animation function with inner-scope interval settings
+    animateTools();
+    let t = setInterval(function () {
+        animateTools();
+    }, 5000);
+
+    $(".tool-item").hover(function () { // w/o this .tool-item:hover css effect would conflict
+        clearInterval(t);
+    }, function () {
+        t = setInterval(function() {
+            animateTools();
+        }, 5000);
+    });
+
+}
+
+function animateTools() { // individual tool animation function
+    let tools = $(".tool-item");
+
+    tools.each(function(index, value) {
+        let element = $(this);
+        setTimeout(function () { // Awesome visual wave-effect occurrence
+            element.addClass("animateTool")
+        }, 250 * index);
+        setTimeout(function () { // Awesome code wave-effect occurrence
+            element.removeClass("animateTool")
+        }, 1000 + (index * 250));
+    })
+
+    console.log("hello");
+}
+
 
 function progress () {
     let meters = $(".skill-meter");
